bd4818a080be07a9356aae775edda92eaa17b47b
Shah Muhammad Jannatul Hasan <58700965+mdjannatulhasan@users.noreply.github.com>
2021-06-24 12:39:07 +0600

    1

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,10 +5,32 @@ module.exports = {
   ],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    container:{
+      center:'true',
+      padding: '2rem'
+    },
+    screens: {
+      sm: '640px',
+      md: '768px',
+      lg: '1024px',
+      xl: '1314px',
+    },
+    extend: {
+      colors:{
+        primary:'#FE4A51',
+        heading: '#2D2D2D',
+        body: '#888888',
+        grayShed: ',rgba(0, 0, 0, 0.63)',
+      },
+      backgroundImage: theme => ({
+        'hero-bg': "linear-gradient(to right bottom, rgba(0, 0, 0,0.63), rgba(0, 0, 0,0.63)),url('../images/hero.png')",
+       })
+    },
   },
   variants: {
-    extend: {},
+    extend: {
+      
+    },
   },
   plugins: [],
 }
